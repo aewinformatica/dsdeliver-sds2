f01293773d5819d81217230265693844b5d55526
AewInformatica <aewinformatica@gmail.com>
2021-01-10 22:15:06 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,24 @@
+import './styles.css';
+import {ReactComponent as Logo} from './logo.svg';
+import {ReactComponent as YouTubeIcon} from './youtube.svg';
+import {ReactComponent as LinkedInIcon} from './linkedin.svg';
+import {ReactComponent as InstagramIcon} from './instagram.svg';
+function Footer(){
+  return (
+    <footer className="main-footer">
+    App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+    <div className="footer-icons">
+      <a href="" target="_new">
+        <YouTubeIcon />
+      </a>
+      <a href="" target="_new">
+        <LinkedInIcon />
+      </a>
+      <a href="" target="_new">
+        <InstagramIcon />
+      </a>
+    </div>
+    </footer>
+  )
+}
+export default Footer;
